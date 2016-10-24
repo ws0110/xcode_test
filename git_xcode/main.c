a498446e66adc8ced130d2fb3eae18a0408e0e0b
ws0110 <ws010110@gmail.com>
2016-10-24 23:29:50 +0900

    Initial Commit

---
+++ git_xcode/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  git_xcode
+//
+//  Created by JangWiSu on 2016. 10. 24..
+//  Copyright © 2016년 JangWiSu. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
