cbe5138f6496b193509fadc93eb639264875b558
ws0110 <ws010110@gmail.com>
2016-10-24 23:39:52 +0900

    branch1 commit

--- git_xcode/main.c
+++ git_xcode/main.c
@@ -12,5 +12,6 @@ int main(int argc, const char * argv[]) {
     // insert code here...
     printf("Hello, World!\n");
     printf("Hello, Miku\n");
+    printf("Hello, new Branch\n");
     return 0;
 }
